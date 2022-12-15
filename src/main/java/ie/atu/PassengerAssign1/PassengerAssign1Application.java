cd331067031bb64693e98078ddafde17a6270745
Denis Costello <G00396309@atu.ie>
2022-12-15 18:31:14 +0000

    Using getmapping to list passengers by ID

--- src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
+++ src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
@@ -3,6 +3,7 @@ package ie.atu.PassengerAssign1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,12 @@ public class PassengerAssign1Application {
 				new Passenger("Mr", "Paul2", "1230987632", "08712321", 93),
 				new Passenger("Mr", "Paul3", "129988776532", "09212343", 53));
 		return myPassengers;
+	}
+
+	@GetMapping("/{passengerID}")
+	public Passenger getPassenger(@PathVariable String passengerID) {
+		Passenger myPassenger = new Passenger("Mr", "Denis", "12332145", "09875323", 24);
+		return myPassenger;
 	}
 
 }
