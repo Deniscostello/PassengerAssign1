272d1d2a8ad07f144c4678445eb52af62efd3bc8
Denis Costello <G00396309@atu.ie>
2022-12-15 18:33:58 +0000

    Using getmapping to list passengers by ID

--- src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
+++ src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
@@ -22,15 +22,15 @@ public class PassengerAssign1Application {
 	public List<Passenger> getPassengers()
 	{
 		List<Passenger> myPassengers = List.of(
-				new Passenger("Mr", "Paul", "12345678901", "09871234", 23),
-				new Passenger("Mr", "Paul2", "1230987632", "08712321", 93),
-				new Passenger("Mr", "Paul3", "129988776532", "09212343", 53));
+				new Passenger("Mr", "Paul", "1234567891", "09871234", 23),
+				new Passenger("Mr", "Paul2", "9876543217", "08712321", 93),
+				new Passenger("Mr", "Paul3", "12345673353", "09212343", 53));
 		return myPassengers;
 	}
 
 	@GetMapping("/{passengerID}")
 	public Passenger getPassenger(@PathVariable String passengerID) {
-		Passenger myPassenger = new Passenger("Mr", "Denis", "12332145", "09875323", 24);
+		Passenger myPassenger = new Passenger("Mr", "Paul", "1234567891", "09871234", 23);
 		return myPassenger;
 	}
 
