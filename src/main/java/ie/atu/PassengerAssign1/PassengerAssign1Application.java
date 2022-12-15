886ac57243a97b4c031abedd3c71f5ecd857c4a1
Denis Costello <G00396309@atu.ie>
2022-12-15 18:08:18 +0000

    First Commit

---
+++ src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.PassengerAssign1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerAssign1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerAssign1Application.class, args);
+	}
+
+}
