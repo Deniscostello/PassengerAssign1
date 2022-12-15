c48c30ac32615834638a928ffe9f0b741cfc2e02
Denis Costello <G00396309@atu.ie>
2022-12-15 19:06:31 +0000

    Updated passenger

--- src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
+++ src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
@@ -14,6 +14,12 @@ import java.util.List;
 @RequestMapping("api/passenger")
 public class PassengerAssign1Application {
 
+	PassengerService myService;
+
+	public PassengerAssign1Application(PassengerService myService) {
+		this.myService = myService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PassengerAssign1Application.class, args);
 	}
@@ -21,17 +27,12 @@ public class PassengerAssign1Application {
 	@GetMapping
 	public List<Passenger> getPassengers()
 	{
-		List<Passenger> myPassengers = List.of(
-				new Passenger("Mr", "Paul", "1234567891", "09871234", 23),
-				new Passenger("Mr", "Paul2", "9876543217", "08712321", 93),
-				new Passenger("Mr", "Paul3", "12345673353", "09212343", 53));
-		return myPassengers;
+		return myService.getPassengers();
 	}
 
 	@GetMapping("/{passengerID}")
 	public Passenger getPassenger(@PathVariable String passengerID) {
-		Passenger myPassenger = new Passenger("Mr", "Paul", "1234567891", "09871234", 23);
-		return myPassenger;
+		return myService.getPassenger(passengerID);
 	}
 
 }
---
+++ src/main/java/ie/atu/PassengerAssign1/PassengerRepo.java
@@ -0,0 +1,4 @@
+package ie.atu.PassengerAssign1;
+
+public interface PassengerRepo {
+}
