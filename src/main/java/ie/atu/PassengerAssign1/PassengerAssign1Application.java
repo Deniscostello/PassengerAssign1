f0befc68845a6ecc3f7bb6e54b89ed2a0468b6c6
Denis Costello <G00396309@atu.ie>
2022-12-16 13:14:17 +0000

    Updated service to get passenger by id

--- src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
+++ src/main/java/ie/atu/PassengerAssign1/PassengerAssign1Application.java
@@ -1,5 +1,6 @@
 package ie.atu.PassengerAssign1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
